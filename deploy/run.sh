dab9b192c73eec204e73550ae1b61970d7543766
Julius Cebreros <juliuscebreros@gmail.com>
2020-03-31 00:43:44 +1300

    Add deploy script

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
-echo "I am trying to deploy here"
+echo "Deploying files..."
+scp -i ~/.ssh/id_rsa_circleci fun.html index.html circleci@juliuscebreros.com:/var/www/html
+scp -i ~/.ssh/id_rsa_circleci -r styles scripts circleci@juliuscebreros.com:/var/www/html
 
-ssh -oStrictHostKeyChecking=no -v circleci@juliuscebreros.com
+# TODO: add a timestamp to display deploy time
+
+echo "Done!"
