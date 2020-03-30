6b5b0088627543272d3c46999ccd3c4cfb5c9813
Julius Cebreros <juliuscebreros@gmail.com>
2020-03-31 00:45:35 +1300

    Turn off strict checking

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 echo "Deploying files..."
-scp -i ~/.ssh/id_rsa_circleci fun.html index.html circleci@juliuscebreros.com:/var/www/html
-scp -i ~/.ssh/id_rsa_circleci -r styles scripts circleci@juliuscebreros.com:/var/www/html
+scp -oStrictHostKeyChecking=no -i ~/.ssh/id_rsa_circleci fun.html index.html circleci@juliuscebreros.com:/var/www/html
+scp -oStrictHostKeyChecking=no -i ~/.ssh/id_rsa_circleci -r styles scripts circleci@juliuscebreros.com:/var/www/html
 
 # TODO: add a timestamp to display deploy time
 
