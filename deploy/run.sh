cf60706155536ee4c9828d999c4cf2261e1b53d2
Julius Cebreros <juliuscebreros@gmail.com>
2020-05-23 01:02:47 +1200

    Fix images folder not working

--- deploy/run.sh
+++ deploy/run.sh
@@ -2,7 +2,7 @@
 
 echo "Deploying files..."
 scp -oStrictHostKeyChecking=no -i ~/.ssh/id_rsa_circleci fun.html index.html circleci@juliuscebreros.com:/var/www/html
-scp -oStrictHostKeyChecking=no -i ~/.ssh/id_rsa_circleci -r styles scripts circleci@juliuscebreros.com:/var/www/html
+scp -oStrictHostKeyChecking=no -i ~/.ssh/id_rsa_circleci -r styles images scripts circleci@juliuscebreros.com:/var/www/html
 
 # TODO: add a timestamp to display deploy time
 
